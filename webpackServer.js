0b26155d2222a5b35e1ca6d4bd4fbae10ec4614a
natew <natewienert@gmail.com>
2015-03-25 10:15:49 -0700

    add done watch with output on builds

--- webpackServer.js
+++ webpackServer.js
@@ -14,7 +14,7 @@ function setConfigsBase(config, base) {
     setConfigBase(config, base);
 }
 
-module.exports = function(config, opts) {
+module.exports = function(config, opts, cb) {
   var hostname = opts.hostname || 'localhost';
   var base = 'http://' + hostname + ':' + opts.port + '/';
 
@@ -38,12 +38,32 @@ module.exports = function(config, opts) {
       "\n", webpackDevServerOpts, "\n"
     );
 
-  var webpackServer = new WebpackDevServer(webpack(config), webpackDevServerOpts);
-
-  console.log('Running Webpack...');
-
+  console.log('Building with Webpack...');
   if (opts.debug)
     console.log('Webpack server on http://%s:%s', hostname, opts.port);
+
+  var compiler = webpack(config);
+
+  // done callback
+  compiler.plugin('done', function(stats) {
+    setTimeout(function() {
+      var warnings = stats.compilation.warnings;
+      var firstWarning = warnings && warnings.length && warnings[0];
+
+      if (firstWarning.name === 'CriticalDependenciesWarning') {
+        console.log();
+        console.log('Note: you have a require warning on your routes, this is ok!'.green);
+      }
+
+      console.log();
+      console.log('Build took %s seconds', (stats.endTime - stats.startTime) / 1000);
+
+      if (cb)
+        cb(stats);
+    }, 50);
+  });
+
+  var webpackServer = new WebpackDevServer(compiler, webpackDevServerOpts);
 
   webpackServer.listen(opts.port, hostname);
 };
